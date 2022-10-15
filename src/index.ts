b33615e63dbdffef14dbdfeaed1070b2223844c4
Dennis Kaminski <dennis.kaminski95@gmail.com>
2022-10-15 20:22:49 +0200

    logging

--- src/index.ts
+++ src/index.ts
@@ -14,10 +14,14 @@ const task = async () => {
 if (ENV.BACKUP_CRON_SCHEDULE !== "*") {
     const job = new CronJob(ENV.BACKUP_CRON_SCHEDULE, async () => {
         await task();
+        console.log("Next planned run...")
+        console.log(job.nextDate().toHTTP());
     });
 
     job.start();
     console.log("Backup cron scheduled...")
+    console.log("Next planned run...")
+    console.log(job.nextDate().toHTTP());
 }
 else {
     (async () => await task())();
